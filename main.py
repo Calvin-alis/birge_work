067091fa558c15893d00afaf8c206ce0be2cb8b5
Calvin-alis <alis.demiurg@gmail.com>
2022-06-04 19:08:18 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+def get_application() -> FastAPI:
+    application = FastAPI()
+    return application
+
+
+app = get_application()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'work',
+    version = '0.0.1',
+    author = 'Oleksandr Ukrainets',
+    author_email = 'oleksandr.ukrainets@nure.ua',
+
+    scripts = ['birg_work/main.py']
+
+)
